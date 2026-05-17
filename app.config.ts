1cac21d279b57f73cf6b3a04ca1195dce36f809b
Jordan Kulzer <jbkulzer@gmail.com>
2026-05-17 14:43:25 -0500

    Version update : 1.0.1

--- app.config.ts
+++ app.config.ts
@@ -5,7 +5,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   newArchEnabled: true,
   name: "Pack",
   slug: "pack-app",
-  version: "1.0.0",
+  version: "1.0.1",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   userInterfaceStyle: "automatic",
