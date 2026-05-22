840d6b84b0edea7d36745774c646b1357571d3b6
Jordan Kulzer <jbkulzer@gmail.com>
2026-05-21 23:11:00 -0500

    build increase

--- app.config.ts
+++ app.config.ts
@@ -5,7 +5,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   newArchEnabled: true,
   name: "Pack",
   slug: "pack-app",
-  version: "1.0.1",
+  version: "1.0.2",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   userInterfaceStyle: "automatic",
