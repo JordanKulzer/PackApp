c15498790e3db810e9e4a76555ed0da419677337
Jordan Kulzer <jbkulzer@gmail.com>
2026-05-26 14:29:53 -0500

    bump version to 1.0.3

--- app.config.ts
+++ app.config.ts
@@ -5,7 +5,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   newArchEnabled: true,
   name: "Pack",
   slug: "pack-app",
-  version: "1.0.2",
+  version: "1.0.3",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   userInterfaceStyle: "automatic",
